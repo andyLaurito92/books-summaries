f0aa009fe4fd1bd8d297c44a654430c582d9db6c
Andrés Laurito Giber <andy.laurito@gmail.com>
2026-01-24 01:43:40 +0100

    Starting

--- practical-common-lisp/chapter2-code/hello.lisp
+++ practical-common-lisp/chapter2-code/hello.lisp
@@ -1,4 +1,5 @@
 ;; First, create sly session by C-x S
+;; To open documentation, C-c h over a function/expression
 
 ;; Evaluate using C-x C-e
 (+ 2 2)
@@ -10,3 +11,9 @@
   )
 
 ;; Note: "Sending" lines as in python is not the way?
+
+;; To install libraries, you do it from the REPL
+(ql:quickload :dexador)
+
+;; To use it, you can do
+(dex:get "https://google.com")
