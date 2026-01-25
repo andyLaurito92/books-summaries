e4b1c3e7202adfbac23ca4d3d0cd2cd18946c898
Andrés Laurito Giber <andy.laurito@gmail.com>
2026-01-25 22:39:22 +0100

    dolist and format

--- practical-common-lisp/chapter3-code/db_example.lisp
+++ practical-common-lisp/chapter3-code/db_example.lisp
@@ -1,3 +1,5 @@
+;; Creating a database of books
+
 (list 1 2 3) ;; Create a simple list
 
 (defparameter x
@@ -5,3 +7,18 @@
   ) ;; property list/map
 
 (print (getf x :b))
+
+(defun make-book (title author numpages)
+  (list :title title :author author :numpages numpages))
+
+;; Okay, so far we can create records but where do we store them?
+;; Let's introduce a global variable
+
+(defvar *bookcase* nil) ;; global variables are defined between *
+
+(defun add-record (abook)
+  (push abook *bookcase*)) ;; push is a macro
+
+(defun dump-db ()
+  (dolist (book *bookcase*)
+    (format t "~{~a:~10t~a~%~}~%" book))) ;; What the heck is this doing? :)
--- practical-common-lisp/chapter3-simpledb.md
+++ practical-common-lisp/chapter3-simpledb.md
@@ -1,4 +1,7 @@
 # Starting to play more w/the language
 
 Difference between functions, macros and special operators:
-- 
+- TODO
+
+
+*Note* The last expression in the body is what a function returns
